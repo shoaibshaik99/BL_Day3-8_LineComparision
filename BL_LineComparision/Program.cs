df500529e00567c9a45ea8113e586083a0f57f30
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-14 01:29:15 +0530

    [Shoaib] Feature to check if lines have equal length or not is added

--- BL_LineComparision/Program.cs
+++ BL_LineComparision/Program.cs
@@ -11,22 +11,36 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
-            // A method to calculate the distance between two points
-            // Get the coordinates of the two end points from the user
-            Console.WriteLine("Enter the coordinates of the first point (x1, y1):");
+            // Get the coordinates of the four points from the user
+            Console.WriteLine("Enter the coordinates of the first point of the first line segment (x1, y1):");
             double x1 = Convert.ToDouble(Console.ReadLine());
             double y1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Enter the coordinates of the second point (x2, y2):");
+            Console.WriteLine("Enter the coordinates of the second point of the first line segment (x2, y2):");
             double x2 = Convert.ToDouble(Console.ReadLine());
             double y2 = Convert.ToDouble(Console.ReadLine());
 
-            // Call the Distance method to find the length of the line segment
-            double length = Distance(x1, y1, x2, y2);
+            Console.WriteLine("Enter the coordinates of the first point of the second line segment (x3, y3):");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+            double y3 = Convert.ToDouble(Console.ReadLine());
 
-            // Display the result
-            Console.WriteLine("The length of the line segment is {0}", length);
+            Console.WriteLine("Enter the coordinates of the second point of the second line segment (x4, y4):");
+            double x4 = Convert.ToDouble(Console.ReadLine());
+            double y4 = Convert.ToDouble(Console.ReadLine());
+
+            // Call the Distance method to find the lengths of the two line segments
+            double length1 = Distance(x1, y1, x2, y2);
+            double length2 = Distance(x3, y3, x4, y4);
+
+            // Compare the lengths and display the result
+            if (length1 == length2)
+            {
+                Console.WriteLine("The two line segments have the same length.");
+            }
+            else
+            {
+                Console.WriteLine("The two line segments do not have the same length.");
+            }
         }
     }
-
 }
