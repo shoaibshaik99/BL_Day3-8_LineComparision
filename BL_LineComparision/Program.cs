47af2a902ee951b0f8cad4accab547df8ac18706
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-14 01:14:26 +0530

    [Shoaib] First sync of local & remote repositories

---
+++ BL_LineComparision/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BL_LineComparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
